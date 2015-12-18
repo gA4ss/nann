fb6105d8050d28c187447340abbc9ec21c9362dc
devilogic <logic.yan@me.com>
2015-12-18 14:08:31 +0800

    修复了matrix左乘，右乘的重大bug

--- inc/nlang.h
+++ inc/nlang.h
@@ -14,6 +14,10 @@ namespace nlang {
 #define NLANG_TYPE_STRING           3
 #define NLANG_TYPE_ARRAY            4
 #define NLANG_TYPE_OBJECT           5
+#define NLANG_TYPE_PROCEDURE        6
+#define NLANG_TYPE_PROCEDURE_PLUGIN 61
+#define NLANG_TYPE_PROCEDURE_LISP   62
+#define NLANG_TYPE_MAP              7
   
   
 #define NLANG_ERROR_LOGIC                           0x82300000
@@ -21,6 +25,34 @@ namespace nlang {
 #define NLANG_ERROR_LOGIC_SYMBOL_NOT_FOUND          0x82300002
 #define NLANG_ERROR_LOGIC_SYMBOL_TYPE_NOT_MATCHED   0x82300003
 
+  class nlang_object;
+  
+#if 0
+  class nlang_procedure : public nanan::nan_object {
+  public:
+    nlang_procedure();
+    virtual ~nlang_procedure();
+    
+  public:
+    int type;
+    std::string name;
+    size_t argc;
+    std::shared_ptr<nlang_object> parents;
+  };
+  
+  class nlang_graph : public nanan::nan_object {
+  public:
+    nlang_graph();
+    virtual ~nlang_graph();
+    
+  public:
+    std::string name;
+    std::string context;
+    std::shared_ptr<nlang_object> parents;
+    std::vector<std::shared_ptr<nlang_object> > map;
+  };
+#endif
+  
   class nlang_object : public nanan::nan_object {
   public:
     nlang_object();
@@ -41,6 +73,10 @@ namespace nlang {
       bool bool_v;
       double double_v;
       std::string string_v;
+#if 0
+      nlang_procedure procedure_v;
+      nlang_graph graph_v;
+#endif
     } nlang_symbol_value_t;
     
     nlang_symbol_value_t value;
@@ -96,6 +132,8 @@ namespace nlang {
     const char *parse_number(nlang_symbol_ptr sym, const char *num);
     const char *parse_array(nlang_symbol_ptr sym, const char *value);
     const char *parse_object(nlang_symbol_ptr sym, const char *value);
+    const char *parse_procedure(nlang_symbol_ptr sym, const char *procedure);
+    const char *parse_graph(nlang_symbol_ptr sym, const char *graph);
     
   private:
     std::map<std::string, nlang_symbol_ptr> _symbols;
